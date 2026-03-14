127891fdadbbb7ba93ff39966e71b721741e4326
Firas <97522852+fifolio@users.noreply.github.com>
2026-03-14 15:03:18 +0000

    Nested Routes

---
+++ app/blog/first/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+  return (
+    <div>1st blog page</div>
+  )
+}
---
+++ app/blog/second/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+    return (
+        <div>2nd blog page</div>
+    )
+}
