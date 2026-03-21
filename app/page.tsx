8125b78634cf27ee9ef50f1c08f9536574df9e60
Firas <97522852+fifolio@users.noreply.github.com>
2026-03-21 17:15:38 +0000

    Link components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
+
 export default function Home() {
   return (
-    <div>Welcome Home!</div>
+    <>
+      <div>Welcome Home!</div>
+      <Link href="/blog">Go to Blog Page</Link>
+      <br />
+      <Link href="/products">Go to Products Page</Link>
+    </>
   )
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,28 @@
+import Link from "next/link";
+
 export default function Products() {
+
+    const productId = 100;
+
     return (
         <>
+            <Link href="/">Home</Link>
             <h1>products page</h1>
-            <p>product 1</p>
-            <p>product 2</p>
-            <p>product 3</p>
-            <p>product 4</p>
+            <div>
+                <Link href="/products/1">product 1</Link>
+            </div>
+            <div>
+                <Link href="/products/2">product 2</Link>
+            </div>
+            <div>
+                <Link href="/products/3" replace>product 3</Link> {/* replace: true -> replaces the current entry in the history stack instead of adding a new one */}
+            </div>
+            <div>
+                <Link href="/products/4">product 4</Link>
+            </div>
+            <div>
+                <Link href={`/products/${productId}`}>product {productId}</Link>
+            </div>
         </>
     )
 }
