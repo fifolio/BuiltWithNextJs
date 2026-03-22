2e6468a2cfc0a7da547fcdbc5fb34fe74a4e42fe
Firas <97522852+fifolio@users.noreply.github.com>
2026-03-22 15:54:45 +0000

    Error Handling

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -3,7 +3,17 @@ import {
     redirect
 } from "next/navigation"
 
+function getRandomInt(count: number){
+    return Math.floor(Math.random() * count);
+}
+
 export default async function ReviewPage({ params }: { params: Promise<{ reviewId: string, productId: string }> }) {
+
+    const random = getRandomInt(2);
+    if(random === 1){
+        throw new Error("Error loading!")
+    }
+
     const { reviewId, productId } = await params
     if (parseInt(reviewId) > 1000) {
         // notFound();
