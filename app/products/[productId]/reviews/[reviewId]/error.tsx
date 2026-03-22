e22c21d34cc7084903455dc32806addf7b40e834
Firas <97522852+fifolio@users.noreply.github.com>
2026-03-22 16:03:21 +0000

    Recovering from Error

--- app/products/[productId]/reviews/[reviewId]/error.tsx
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,21 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
+export default function ErrorBoundary({ error, reset }: { error: Error, reset: () => void }) {
+
+    const router = useRouter();
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        })
+    }
+
     return (
-        <div>{error.message}</div>
+        <div>
+            <p>{error.message}</p>
+            <button onClick={reload}>Try again</button>
+        </div>
     )
 }
