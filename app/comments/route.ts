20729d2fc033720262c400f7d5974248bc4f083b
Firas <97522852+fifolio@users.noreply.github.com>
2026-04-06 17:37:37 +0000

    Handling POST Requests

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -3,3 +3,18 @@ import { comments } from './data';
 export async function GET() {
     return Response.json(comments)
 }
+
+export async function POST(request: Request){
+    const comment = await request.json()
+    const newComment = {
+        id: Number(comments.length),
+        text: comment.text,
+    };
+
+    comments.push(newComment);
+
+    return new Response(JSON.stringify(newComment), {
+        headers: {"Content-Type": "application/json"}, 
+        status: 201, 
+    })
+}
