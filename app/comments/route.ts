a6983c8bdfdf83e32e46d23a7ba3be258336d844
Firas <97522852+fifolio@users.noreply.github.com>
2026-04-09 15:39:07 +0000

    URL Query Parameters

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,10 +1,16 @@
+import { NextRequest } from 'next/server';
 import { comments } from './data';
 
-export async function GET() {
-    return Response.json(comments)
+export async function GET(request: NextRequest) {
+
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('query');
+    const filteredComments = query ? comments.filter(c => c.text.includes(query)) : comments;
+
+    return Response.json(filteredComments)
 }
 
-export async function POST(request: Request){
+export async function POST(request: Request) {
     const comment = await request.json()
     const newComment = {
         id: Number(comments.length),
@@ -14,7 +20,7 @@ export async function POST(request: Request){
     comments.push(newComment);
 
     return new Response(JSON.stringify(newComment), {
-        headers: {"Content-Type": "application/json"}, 
-        status: 201, 
+        headers: { "Content-Type": "application/json" },
+        status: 201,
     })
 }
